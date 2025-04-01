dca34e090f7d903995d6313eedf63676e5e36dd9
tywangq <158242974+tywangq@users.noreply.github.com>
2025-04-01 16:39:02 -0700

    unit tests for accounts

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq" // 手动导入；且需要_
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:psql@localhost:5432/bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
---
+++ util/random.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+// func init() {
+// 	rand.Seed(time.Now().UnixNano())
+// }
+
+func RandomInt(min, max int64) int64 {
+	return min + rnd.Int63n(max-min+1) // min + [0, max-min+1) => [min, max]
+}
+
+func RandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = alphabet[rnd.Intn(len(alphabet))]
+	}
+	return string(b)
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomBalance() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[RandomInt(0, int64(n)-1)]
+}
