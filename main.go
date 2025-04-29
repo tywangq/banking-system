77e5b5a4c0e2b618fff19a93876fc672a2585e45
tywangq <158242974+tywangq@users.noreply.github.com>
2025-04-29 10:04:38 -0700

    feat: add gRPC server implementation and update configuration for HTTP and gRPC addresses

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	"github.com/tywangq/banking-system/api"
 	db "github.com/tywangq/banking-system/db/sqlc"
+	"github.com/tywangq/banking-system/gapi"
+	"github.com/tywangq/banking-system/pb"
 	"github.com/tywangq/banking-system/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	_ "github.com/lib/pq" // 手动导入；且需要_
 )
@@ -23,12 +28,38 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	runGrpcServer(config, store) // 此处可更换为runHttpServer
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server")
+	}
+}
+
+func runHttpServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create server:", err)
 	}
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
